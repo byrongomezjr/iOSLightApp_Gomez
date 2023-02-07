130354c966c6e6b31e1e5363e187847400578450
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-02-07 12:35:40 -0500

    Initial Commit

---
+++ iOSLightApp_Gomez/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSLightApp_Gomez
+//
+//  Created by Byron Gomez Jr on 2/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
